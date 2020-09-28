800df09f961e4fc662de2ea428bb7439fef07019
billbo99 <cactus_computers-github@yahoo.com>
2020-09-28 09:09:13 +0100

    0.1.0

--- dist/control.lua
+++ dist/control.lua
@@ -1,10 +1,38 @@
+require "util"
+
 local function CurrentLevel(param)
     return (math.sqrt(param + 25) + 5) / 10
 end
 
+local function ReApplyBonus(player)
+    if player.controller_type == defines.controllers.character then
+        player.character_crafting_speed_modifier = (math.floor(CurrentLevel(global.players[player.name].crafting.count)) - 1) * 0.1
+        player.character_mining_speed_modifier = (math.floor(CurrentLevel(global.players[player.name].mining.count)) - 1) * 0.1
+    end
+end
+
+local function FixPlayerRecord(player)
+    if global.players[player.name] == nil then
+        global.players[player.name] = {}
+    end
+    if global.players[player.name].crafting == nil then
+        global.players[player.name].crafting = {count = 0, level = 1}
+    end
+    if global.players[player.name].mining == nil then
+        global.players[player.name].mining = {count = 0, level = 1}
+    end
+    ReApplyBonus(player)
+end
+
+local function ReApplyBonuses(e)
+    for ixd, _ in pairs(game.connected_players) do
+        local player = game.get_player(ixd)
+        FixPlayerRecord(player)
+    end
+end
+
 local function OnInit(e)
-    global.crafting = {}
-    global.mining = {}
+    global.players = global.players or {}
     global.print_colour = {r = 255, g = 255, b = 0}
 end
 
@@ -13,52 +41,77 @@ end
 
 local function OnConfigurationChanged(e)
     if e.mod_changes and e.mod_changes["DoingThingsByHand"] then
-        global.crafting = global.crafting or {}
-        global.mining = global.mining or {}
+        -- migrate to new dict structure
+        if global.players == nil then
+            global.players = {}
+            for idx, _ in pairs(global.crafting) do
+                local player = game.get_player(idx)
+                if player then
+                    if global.players[player.name] == nil then
+                        global.players[player.name] = {}
+                    end
+                    global.players[player.name].crafting = table.deepcopy(global.crafting[idx])
+                end
+            end
+            for idx, _ in pairs(global.mining) do
+                local player = game.get_player(idx)
+                if player then
+                    if global.players[player.name] == nil then
+                        global.players[player.name] = {}
+                    end
+                    global.players[player.name].mining = table.deepcopy(global.mining[idx])
+                end
+            end
+            if global.crafting then
+                global.crafting = nil
+            end
+            if global.mining then
+                global.mining = nil
+            end
+        end
         global.print_colour = {r = 255, g = 255, b = 0}
     end
 end
 
 local function OnPlayerMinedEntity(e)
-    global.mining[e.player_index] = global.mining[e.player_index] or {count = 0, level = 1}
-    local playerMining = global.mining[e.player_index]
+    local player = game.get_player(e.player_index)
+    global.players[player.name].mining = global.players[player.name].mining or {count = 0, level = 1}
+
+    local playerMining = global.players[player.name].mining
     playerMining.count = playerMining.count + 1
 
     local current_level = math.floor(CurrentLevel(playerMining.count))
 
     if current_level ~= playerMining.level then
         playerMining.level = current_level
-        local player = game.get_player(e.player_index)
         player.character_mining_speed_modifier = (playerMining.level - 1) * 0.1
         player.print("Mining speed bonus has now been increased to .. " .. tostring(player.character_mining_speed_modifier * 100) .. "%", global.print_colour)
     end
 end
 
 local function OnPlayerCraftedItem(e)
-    global.crafting[e.player_index] = global.crafting[e.player_index] or {count = 0, level = 1}
-    local playerCrafting = global.crafting[e.player_index]
+    local player = game.get_player(e.player_index)
+    global.players[player.name].crafting = global.players[player.name].crafting or {count = 0, level = 1}
+
+    local playerCrafting = global.players[player.name].crafting
     playerCrafting.count = playerCrafting.count + 1
 
     local current_level = math.floor(CurrentLevel(playerCrafting.count))
 
     if current_level ~= playerCrafting.level then
         playerCrafting.level = current_level
-        local player = game.get_player(e.player_index)
         player.character_crafting_speed_modifier = (playerCrafting.level - 1) * 0.1
         player.print("Crafting speed bonus has now been increased to .. " .. tostring(player.character_crafting_speed_modifier * 100) .. "%", global.print_colour)
     end
 end
 
-local function ReApplyBonuses(e)
-    for ixd, _ in pairs(game.connected_players) do
-        local playerCrafting = global.crafting[ixd]
-        local playerMining = global.mining[ixd]
-        local player = game.get_player(ixd)
-        if player.controller_type == defines.controllers.character then
-            player.character_crafting_speed_modifier = (math.floor(CurrentLevel(playerCrafting.count)) - 1) * 0.1
-            player.character_mining_speed_modifier = (math.floor(CurrentLevel(playerMining.count)) - 1) * 0.1
-        end
-    end
+local function OnPlayerCreated(e)
+    local player = game.get_player(e.player_index)
+    FixPlayerRecord(player)
+end
+
+local function OnPlayerJoinedGame(e)
+    OnPlayerCreated(e)
 end
 
 commands.add_command(
@@ -71,21 +124,21 @@ commands.add_command(
             player = game.get_player(event.parameter)
         end
 
-        local playerCrafting = global.crafting[player.index]
-        local playerMining = global.mining[player.index]
+        FixPlayerRecord(player)
+        local playerCrafting = global.players[player.name].crafting
+        local playerMining = global.players[player.name].mining
 
-        calling_player.print(string.format("Mining Level .. %2.2f", CurrentLevel(playerMining.count)), global.print_colour)
-        calling_player.print("Mining Bonus .. " .. tostring(player.character_mining_speed_modifier * 100) .. "%", global.print_colour)
-        calling_player.print(string.format("Crafting Level .. %2.2f", CurrentLevel(playerCrafting.count)), global.print_colour)
-        calling_player.print("Crafting Bonus .. " .. tostring(player.character_crafting_speed_modifier * 100) .. "%", global.print_colour)
+        calling_player.print(string.format("Mining .. (Level .. %2.2f) .. (Bonus %d%%)", CurrentLevel(playerMining.count), player.character_mining_speed_modifier * 100), global.print_colour)
+        calling_player.print(string.format("Crafting .. (Level .. %2.2f) .. (Bonus %d%%)", CurrentLevel(playerCrafting.count), player.character_crafting_speed_modifier * 100), global.print_colour)
     end
 )
 
 script.on_init(OnInit)
 script.on_load(OnLoad)
 script.on_configuration_changed(OnConfigurationChanged)
-script.on_nth_tick(300, ReApplyBonuses)
+script.on_nth_tick(1800, ReApplyBonuses)
 script.on_event(defines.events.on_player_crafted_item, OnPlayerCraftedItem)
 script.on_event(defines.events.on_player_mined_entity, OnPlayerMinedEntity)
-script.on_event(defines.events.on_player_joined_game, ReApplyBonuses)
+script.on_event(defines.events.on_player_joined_game, OnPlayerJoinedGame)
 script.on_event(defines.events.on_player_respawned, ReApplyBonuses)
+script.on_event(defines.events.on_player_created, OnPlayerCreated)
